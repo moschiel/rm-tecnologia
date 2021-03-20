1aa10f9e5cb520f955f82b0ba51507625c702ae4
Roger da Silva Moschiel-ROGERM <rogerm@motorola.com>
2021-03-20 15:07:58 -0300

    pass retirado do repositorio

--- enviar-email.php
+++ enviar-email.php
@@ -11,17 +11,19 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 $mail->IsSMTP(); // enable SMTP
 
 
-//$mail->SMTPDebug = 2;
+// $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 //$mail->Debugoutput = 'html';
 
-// $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
 $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "br118.hostgator.com.br";
 $mail->Port = 465; // or 587
 $mail->IsHTML(true);
 $mail->Username = "contato@rmsoftwarehouse.com";
-$mail->Password = "wk15nj8dsp";
+
+$myfile = fopen("./pass.txt", "r") or die("Unable to open file!");
+$mail->Password = fgets($myfile);
+fclose($myfile);
  
 $mail->SetFrom($email, $nome);
 $mail->Subject = "Assunto da mensagem";
